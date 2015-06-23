e6807bd9de49006ed1b0a2a7a41a98d8caf14a32
Mishit Vora <mvora62@gmail.com>
2015-06-23 18:07:52 +0530

    Change Name JobPost

---
+++ HomeWork1/src/homework1/model/JobPost.java
@@ -0,0 +1,24 @@
+package homework1.model;
+
+import java.util.Date;
+
+public class JobPost {
+	
+	String Job;
+	Date Postingjob;
+	public String getJob() {
+		return Job;
+	}
+	public void setJob(String job) {
+		Job = job;
+	}
+	public Date getPostingjob() {
+		return Postingjob;
+	}
+	public void setPostingjob(Date postingjob) {
+		Postingjob = postingjob;
+	}
+	
+	
+
+}
